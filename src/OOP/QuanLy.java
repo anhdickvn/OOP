fa00e42844994aff064ea0d3857ee899bce4b9f9
anhho7303-source <anhho7303@gmail.com>
2025-10-26 02:59:15 +0700

    Update QuanLy.java

--- src/OOP/QuanLy.java
+++ src/OOP/QuanLy.java
@@ -43,24 +43,41 @@ public class QuanLy extends ConNguoi {
 	}
 
 	@Override
-	public void nhapThongTin() {
-		super.nhapThongTin();
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Nhap ID quan ly: ");
-		idQuanLy = sc.nextLine();
-		System.out.print("Nhap luong: ");
-		luong = sc.nextDouble();
-		sc.nextLine();
-		System.out.print("Nhap khu lam viec: ");
-		khuLamViec = sc.nextLine();
-	}
+	public static void xuatQuanLy() {
+    QuanLy dsQL[] = new QuanLy[0];
+    try {
+        BufferedReader br = new BufferedReader(new FileReader("QuanLy.txt"));
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] st = line.split(";");
 
-	@Override
-	public void hienThiThongTin() {
-		System.out.println("=== Thong tin Quan Ly ===");
-		super.hienThiThongTin();
-		System.out.println("ID Quan Ly: " + idQuanLy);
-		System.out.println("Luong: " + luong);
-		System.out.println("Khu lam viec: " + khuLamViec);
-	}
+            QuanLy ql = new QuanLy(
+                st[0],st[1],st[2],st[3],st[4],  // Thuộc tính con người               
+                st[5],                 // idQuanLy
+                Double.parseDouble(st[6]), // luong
+                st[7]                  // khuLamViec
+            );
+
+            dsQL = java.util.Arrays.copyOf(dsQL, dsQL.length + 1);
+            dsQL[dsQL.length - 1] = ql;
+        }
+        br.close();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+
+    System.out.printf("%-20s| %-15s| %-12s| %-15s| %-12s| %-10s| %-12s| %-15s\n",
+        "Ho Ten", "Ma Dinh Danh", "Ngay Sinh", "Dia Chi", "SDT",
+        "ID QL", "Luong", "Khu Lam Viec");
+    System.out.println("-----------------------------------------------------------------------------------------------------------");
+    for (QuanLy ql : dsQL) {
+        System.out.println(ql);
+    }
+}
+@Override
+public String toString() {
+    return String.format("%-20s| %-15s| %-12s| %-15s| %-12s| %-10s| %-12.0f| %-15s",
+        getHoTen(), getMaDinhDanh(), getNgaySinh(), getDiaChi(), getSoDienThoai(),
+        idQuanLy, luong, khuLamViec);
+}
 }
