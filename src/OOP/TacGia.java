b38599da9541612ecc6c69ea7602d41f207a336b
anhdickvn <107830708+anhdickvn@users.noreply.github.com>
2025-10-26 01:04:30 +0700

    Update TacGia.java

--- src/OOP/TacGia.java
+++ src/OOP/TacGia.java
@@ -1,11 +1,8 @@
 package OOP;
 
-import java.util.Scanner;
-
 public class TacGia {
 	private String idTacGia;
 	private String tenTacGia;
-	private String ngaySinh;
 
 	public String getIdTacGia() {
 		return this.idTacGia;
@@ -23,36 +20,15 @@ public class TacGia {
 		this.tenTacGia = tenTacGia;
 	}
 
-	public String getNgaySinh() {
-		return this.ngaySinh;
-	}
-
-	public void setNgaySinh(String ngaySinh) {
-		this.ngaySinh = ngaySinh;
-	}
-
 	public TacGia() {
 	}
 
-	public TacGia(String idTacGia, String tenTacGia, String ngaySinh) {
+	public TacGia(String idTacGia, String tenTacGia) {
 		this.idTacGia = idTacGia;
 		this.tenTacGia = tenTacGia;
-		this.ngaySinh = ngaySinh;
 	}
 
-	public void nhap() {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("ID tac gia: ");
-		idTacGia = sc.nextLine();
-		System.out.print("Ten tac gia: ");
-		tenTacGia = sc.nextLine();
-		System.out.println("Ngay sinh: ");
-		ngaySinh = sc.nextLine();
-	}
-
-	public void xuat() {
-		System.out.printf("%-15s %-25s %-15s %-20s\n", "ID Tác Giả", "Tên Tác Giả", "Ngày Sinh");
-		System.out.println("--------------------------------------------------------------------------");
-		System.out.printf("%-15s %-25s %-15s %-20s\n", idTacGia, tenTacGia, ngaySinh);
+	public String toString() {
+		return idTacGia + " - " + tenTacGia;
 	}
 }
