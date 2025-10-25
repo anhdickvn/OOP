1aa01b4df4076995c29abe4da739bfe0b81f5f16
ThanhTam165 <thanhtampro533@gmail.com>
2025-10-25 22:28:00 +0700

    Update KhachHangVIP.java

--- src/OOP/KhachHangVIP.java
+++ src/OOP/KhachHangVIP.java
@@ -7,11 +7,12 @@ class KhachHangVIP extends KhachHang {
 	public KhachHangVIP() {
 	}
 
-	public KhachHangVIP(String hoTen, String maDinhDanh, String ngaySinh, String diaChi, String soDienThoai,
+	public KhachHangVIP(String hoTen, String ngaySinh, String diaChi, String soDienThoai,
 			String idKhachHang, double mucGiamGia, int diemTichLuy, double tongTienMua) {
-		super(hoTen, maDinhDanh, ngaySinh, diaChi, soDienThoai, idKhachHang);
+		super(hoTen, ngaySinh, diaChi, soDienThoai, idKhachHang);
 		this.mucGiamGia = mucGiamGia;
 		this.diemTichLuy = diemTichLuy;
+		this.tongTienMua = tongTienMua;
 	}
 
 	public double getMucGiamGia() {
@@ -29,35 +30,38 @@ class KhachHangVIP extends KhachHang {
 	public void setDiemTichLuy(int diemTichLuy) {
 		this.diemTichLuy = diemTichLuy;
 	}
-	public double getTongTienMua(){
+
+	public double getTongTienMua() {
 		return tongTienMua;
 	}
-	public void setTongTienMua(double tongTienMua) {
-		this.tongTienMua=tongTienMua;
 
+	public void setTongTienMua(double tongTienMua) {
+		this.tongTienMua = tongTienMua;
 	}
-  // if diem tich luy >100  muc gia 15% 
-  // if diem tich luy >0 && <=50 muc giam gia 5%
-  // if diem tich luy >50&&<=100 muc giam gia 10%
+
 	@Override
 	public void nhapThongTin() {
 		super.nhapThongTin();
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Nhap tong so tien da mua (VND):");
-		tongTienMua=sc.nextDouble();
-		diemTichLuy=(int)(tongTienMua/1000);
-		if (diemTichLuy > 100) mucGiamGia = 0.15;
-		else if (diemTichLuy > 50 && diemTichLuy <= 100) mucGiamGia = 0.1;
-		else if (diemTichLuy > 0 && diemTichLuy <= 50) mucGiamGia = 0.05;
-		System.out.println("Muc giam gia hien tai"+(mucGiamGia*100)+"%");
+		System.out.print("Nhap tong so tien da mua (VND): ");
+		tongTienMua = sc.nextDouble();
+		diemTichLuy = (int) (tongTienMua / 1000);
+
+		if (diemTichLuy > 100)
+			mucGiamGia = 0.15;
+		else if (diemTichLuy > 50 && diemTichLuy <= 100)
+			mucGiamGia = 0.1;
+		else if (diemTichLuy > 0 && diemTichLuy <=50)
+			mucGiamGia = 0.05;
+		System.out.println("Muc giam gia hien tai: " + (mucGiamGia * 100) + "%");
 	}
 
 	@Override
-    public void hienThiThongTin() {
-        System.out.println("\n=== Khach Hang VIP ===");
-        super.hienThiThongTin();
-        System.out.println("Tong tien da mua: " + tongTienMua + " VND");
-        System.out.println("Diem tich luy: " + diemTichLuy);
-        System.out.println("Muc giam gia hien tai: " + mucGiamGia*100 +"%");
-    }
+	public void hienThiThongTin() {
+		System.out.println("\n=== Khach Hang VIP ===");
+		super.hienThiThongTin();
+		System.out.println("Tong tien da mua: " + tongTienMua + " VND");
+		System.out.println("Diem tich luy: " + diemTichLuy);
+		System.out.println("Muc giam gia hien tai: " + (mucGiamGia * 100) + "%");
+	}
 }
