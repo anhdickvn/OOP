d03cf5689a4d0c5ad10512b3173fbb9d480da00d
anhdickvn <107830708+anhdickvn@users.noreply.github.com>
2025-10-26 23:34:27 +0700

    Update KhachHang.java

--- src/OOP/KhachHang.java
+++ src/OOP/KhachHang.java
@@ -1,12 +1,12 @@
 package OOP;
 
-class KhachHang extends ConNguoi {
+public class KhachHang extends ConNguoi {
 	private String idKhachHang;
 
 	public KhachHang() {
 	}
 
-	public KhachHang(String hoTen, String ngaySinh, String diaChi, String soDienThoai, String idKhachHang) {
+	public KhachHang(String idKhachHang, String hoTen, String ngaySinh, String diaChi, String soDienThoai) {
 		super(hoTen, ngaySinh, diaChi, soDienThoai);
 		this.idKhachHang = idKhachHang;
 	}
@@ -20,17 +20,7 @@ class KhachHang extends ConNguoi {
 	}
 
 	@Override
-	public void nhapThongTin() {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Nhap ID khach hang: ");
-		idKhachHang = sc.nextLine();
-		super.nhapThongTin();
-	}
-
-	@Override
-	public void hienThiThongTin() {
-		System.out.println("ID khach hang: " + idKhachHang);
-		super.hienThiThongTin();
+	public String toString() {
+		return idKhachHang + " | " + super.toString();
 	}
 }
-
