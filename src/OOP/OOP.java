9261d9e217c421ed2e44f42f745ec76a8b765bfe
anhho7303-source <anhho7303@gmail.com>
2025-10-28 00:58:03 +0700

    Update OOP.java

--- src/OOP/OOP.java
+++ src/OOP/OOP.java
@@ -3,5 +3,15 @@ package OOP;
 public class OOP {
 	public static void main(String[] args) {
 		Sach.xuatKho();
+		ThongKeNhanVien tk = new ThongKeNhanVien();
+ThuongNhanVien thuong = new ThuongNhanVien();
+NhanVien.xuatNhanVien();
+QuanLy.xuatQuanLy();
+tk.thongKeNhanVienTheoDoanhThu();
+tk.thongKeTheoChucVu();
+tk.thongKeTuoiTangDan();
+thuong.tinhThuong();
+DanhSachConNguoi.ChucNangNhanVien();
+DanhSachConNguoi.ChucNangQuanLy();
 	}
 }
